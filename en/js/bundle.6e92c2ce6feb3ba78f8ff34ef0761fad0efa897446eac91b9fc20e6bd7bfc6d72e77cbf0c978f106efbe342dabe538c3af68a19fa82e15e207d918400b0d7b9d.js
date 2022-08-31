ec08e900316a9f75c03dc6f031abd0e5f65d4258
dotdc <dotdc@users.noreply.github.com>
2022-08-31 06:44:15 +0000

    deploy: 0847de16b52ae46eba858734e0b4cd884a89481a

--- en/js/bundle.6e92c2ce6feb3ba78f8ff34ef0761fad0efa897446eac91b9fc20e6bd7bfc6d72e77cbf0c978f106efbe342dabe538c3af68a19fa82e15e207d918400b0d7b9d.js
+++ en/js/bundle.6e92c2ce6feb3ba78f8ff34ef0761fad0efa897446eac91b9fc20e6bd7bfc6d72e77cbf0c978f106efbe342dabe538c3af68a19fa82e15e207d918400b0d7b9d.js
@@ -8,7 +8,7 @@ const scaleImageClass = 'image-scale';
 const pageHasLoaded = 'DOMContentLoaded';
 const imageAltClass = 'img_alt'
 
-const baseURL = 'http://0xDC.me/';
+const baseURL = 'https://dotdc.github.io/';
 const searchFieldClass = '.search_field';
 const searchClass = '.search';
 const goBackClass = 'button_back';
